a0cc06834ab6973fd31ba068ac2372c107818ca4
joesatow <98419512+joesatow@users.noreply.github.com>
2022-09-29 15:41:21 -0400

    changes

--- helperFuncs.py
+++ helperFuncs.py
@@ -1,9 +1,12 @@
 import math
 import requests
+import os
+from requests import Response
 from datetime import datetime, timedelta
-from urllib.parse import urlencode, urlparse
+from urllib.parse import urlencode
 from user_agent import generate_user_agent
 
+user_agent = generate_user_agent()
 def timestamp(dt):
     epoch = datetime.utcfromtimestamp(0)
     return (dt - epoch).total_seconds() * 1000.0
@@ -36,7 +39,28 @@ def get_chart(period, size, chart_type,ta):
     encoded_payload = urlencode(
             {"ty": chart_type, "ta": ta, "p": period, "s": size}
         )
+    
+    data_scrape(
+        download_chart_image, 
+        f"https://finviz.com/chart.ashx?{encoded_payload}&t={ticker}",
+        user_agent
+    )
 
 def finviz_request(url: str, user_agent: str) -> Response:
     response = requests.get(url, headers={"User-Agent": user_agent})
     return response
+
+def download_chart_image(page_content: requests.Response, **kwargs):
+    """ Downloads a .png image of a chart into the "charts" folder. """
+    file_name = f"{kwargs['URL'].split('t=')[1]}_{int(time.time())}.png"
+
+    if not os.path.exists("charts"):
+        os.mkdir("charts")
+
+    with open(os.path.join("charts", file_name), "wb") as handle:
+        handle.write(page_content.content)
+    
+def data_scrape(scrape_func, url, user_agent):
+    response = finviz_request(url, user_agent)
+    kwargs["URL"] = url
+    data.append(scrape_func(response, *args, **kwargs))
