fdc2181a94738dccce1890d4a77e5aa1304a5dee
joesatow <98419512+joesatow@users.noreply.github.com>
2022-09-30 10:16:44 -0400

    changes

--- helper_funcs/dateFunctions.py
+++ helper_funcs/dateFunctions.py
@@ -1,6 +1,9 @@
 import math
 from datetime import datetime, timedelta
 
+# number of days to subtract from current day to use with getting startDate
+subtractedDays = 5
+
 def timestamp(dt):
     epoch = datetime.utcfromtimestamp(0)
     return (dt - epoch).total_seconds() * 1000.0
@@ -9,8 +12,9 @@ currentYear = datetime.now().year
 currentMonth = datetime.now().month
 currentDay = datetime.now().day
 todayMarketCloseTime = datetime(currentYear,currentMonth,currentDay,20,1,0)
+
 endDate = math.trunc(timestamp(todayMarketCloseTime)) # datetime(year, month, day, hour, minute, second)
-startDate = math.trunc(timestamp(todayMarketCloseTime-timedelta(days=2))) # subtract two days
+startDate = math.trunc(timestamp(todayMarketCloseTime-timedelta(days=subtractedDays))) # subtract specified days from endDate to get startDate
 
 def getEndDate():
     return endDate
