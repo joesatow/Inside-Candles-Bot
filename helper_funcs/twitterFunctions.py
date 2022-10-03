7e7e2082f51894722d6dd135a10c64a7c4e670fc
joesatow <joseph.satow@gmail.com>
2022-10-03 19:32:52 -0400

    fix again

--- helper_funcs/twitterFunctions.py
+++ helper_funcs/twitterFunctions.py
@@ -7,7 +7,7 @@ url = "https://api.twitter.com/2/tweets"
 consumer_key = os.environ['api_key']
 access_token = os.environ['access_token']
 headers = {
-  'Authorization': f'OAuth oauth_consumer_key={consumer_key},oauth_token={access_token},oauth_signature_method="HMAC-SHA1",oauth_timestamp="1664722905",oauth_nonce="pls6CnNDIvr",oauth_version="1.0",oauth_signature="QAPeTPhfT5mE1yu2bZ6Gh%2BzTDIQ%3D"',
+  'Authorization': f'OAuth oauth_consumer_key={consumer_key},oauth_token={access_token},oauth_signature_method="HMAC-SHA1",oauth_timestamp="1664839898",oauth_nonce="0WMpEFz7NPB",oauth_version="1.0",oauth_signature="oGtP8Vm6ta2uSqjWltF1bRF42ck%3D"',
   'Content-Type': 'application/json',
   'Cookie': 'guest_id=v1%3A164748160617485457; guest_id_ads=v1%3A164748160617485457; guest_id_marketing=v1%3A164748160617485457; personalization_id="v1_PFHqkQhmwp141oUu4hPm9w=="'
 }
@@ -21,3 +21,4 @@ def sendTweet(tweetText, mediaID):
     })
 
     response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.json())
