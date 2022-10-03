1f39b408c731fdf68452a259d317bc30a79a20f9
joesatow <joseph.satow@gmail.com>
2022-10-03 19:39:00 -0400

    change TD api to env var

--- helper_funcs/TDA_Functions.py
+++ helper_funcs/TDA_Functions.py
@@ -2,10 +2,14 @@ from ratelimit import limits, sleep_and_retry
 from backoff import on_exception, expo
 import math
 import requests
+import os
 from datetime import datetime, timedelta
 
 # number of days to subtract from current day to use with getting startDate
 subtractedDays = 5
+
+# TD API key 
+TD_API_Key = os.environ['td_api_key']
 
 def timestamp(dt):
     epoch = datetime.utcfromtimestamp(0)
@@ -24,7 +28,7 @@ startDate = math.trunc(timestamp(todayMarketCloseTime-timedelta(days=subtractedD
 @sleep_and_retry
 @limits(calls=120, period=60)
 def call_TD_API(symbol):
-    url = f"https://api.tdameritrade.com/v1/marketdata/{symbol}/pricehistory?apikey=KM7SSWFJANTN4HOJIMYUGZAY1C09QWH3&periodType=month&frequencyType=daily&frequency=1&endDate={endDate}&startDate={startDate}"
+    url = f"https://api.tdameritrade.com/v1/marketdata/{symbol}/pricehistory?apikey={TD_API_Key}&periodType=month&frequencyType=daily&frequency=1&endDate={endDate}&startDate={startDate}"
     response = requests.request("GET", url, headers={}, data={})
     response = response.json()
 
