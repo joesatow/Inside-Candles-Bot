8efb25752eb73eb2c7856317e85f10b8faa2bec8
joesatow <98419512+joesatow@users.noreply.github.com>
2022-10-07 15:09:20 -0400

    test hash WORKING

--- testHash.py
+++ testHash.py
@@ -12,59 +12,132 @@ from helper_funcs.API_keys import getKey
 
 url = "https://api.twitter.com/2/tweets"
 
-consumer_key = getKey('api_key')
+consumer_key = getKey('consumer_key')
 consumer_key_secret = getKey('consumer_key_secret')
 access_token = getKey('access_token')
 access_token_secret = getKey('access_token_secret')
+baseURL = "https://api.twitter.com/2/tweets"
+nonce = secrets.token_urlsafe()
+nonce = ''.join(filter(str.isalnum, nonce))
+#nonce = '2e1acZT4H5d'
+timestamp = str(int(time.time()))
+#timestamp = '1665161180'
 
+def percentEncode(str):
+    return urllib.parse.quote(str, safe="")
+
+# consumer_key = 'xvz1evFS4wEEPTGEFPHBog'
+# consumer_key_secret = 'kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw'
+# nonce = "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg"
+# timestamp = "1318622958"
+# access_token = "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb"
+# access_token_secret = "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE"
+# baseURL = "https://api.twitter.com/1.1/statuses/update.json"
+
+oauth_consumer_key_KEY = percentEncode("oauth_consumer_key")
+oauth_consumer_key_VALUE = percentEncode(consumer_key)
+
+oauth_nonce_KEY = percentEncode("oauth_nonce")
+oauth_nonce_VALUE = percentEncode(nonce)
+
+oauth_signature_method_KEY = percentEncode("oauth_signature_method")
+oauth_signature_method_VALUE = percentEncode("HMAC-SHA1")
+
+oauth_timestamp_KEY = percentEncode("oauth_timestamp")
+oauth_timestamp_VALUE = percentEncode(timestamp)
+
+oauth_token_KEY = percentEncode("oauth_token")
+oauth_token_VALUE = percentEncode(access_token)
+
+oauth_version_KEY = percentEncode("oauth_version")
+oauth_version_VALUE = percentEncode("1.0")
+
+include_entities_KEY = percentEncode("include_entities")
+include_entities_VALUE = percentEncode("true")
+
+status_KEY = percentEncode("status")
+status_VALUE = percentEncode("Hello Ladies + Gentlemen, a signed OAuth request!")
+
+#parameterString = include_entities_KEY + '=' + include_entities_VALUE + "&"
+parameterString = oauth_consumer_key_KEY + '=' + oauth_consumer_key_VALUE + '&'
+parameterString += oauth_nonce_KEY + '=' + oauth_nonce_VALUE + '&'
+parameterString += oauth_signature_method_KEY + '=' + oauth_signature_method_VALUE + '&'
+parameterString += oauth_timestamp_KEY + '=' + oauth_timestamp_VALUE + '&'
+parameterString += oauth_token_KEY + '=' + oauth_token_VALUE + '&'
+parameterString += oauth_version_KEY + '=' + oauth_version_VALUE
+#parameterString += status_KEY + '=' + status_VALUE
+
+# parameterString = include_entities_KEY + '=' + include_entities_VALUE + "&"
+# parameterString += oauth_consumer_key_KEY + '=' + oauth_consumer_key_VALUE + '&'
+# parameterString += oauth_nonce_KEY + '=' + oauth_nonce_VALUE + '&'
+# parameterString += oauth_signature_method_KEY + '=' + oauth_signature_method_VALUE + '&'
+# parameterString += oauth_timestamp_KEY + '=' + oauth_timestamp_VALUE + '&'
+# parameterString += oauth_token_KEY + '=' + oauth_token_VALUE + '&'
+# parameterString += oauth_version_KEY + '=' + oauth_version_VALUE + '&'
+# parameterString += status_KEY + '=' + status_VALUE
+
+
+# parameterString = urlencode(
+#     {
+#         "include_entities": "true",
+#         "oauth_consumer_key": consumer_key,
+#         "oauth_nonce": nonce,
+#         "oauth_signature_method": 'HMAC-SHA1',
+#         "oauth_timestamp": timestamp,
+#         "oauth_token": access_token,
+#         "oauth_version": "1.0",
+#         "status": percentEncode(b"Hello Ladies + Gentlemen, a signed OAuth request!")
+#     },
+#     quote_via=quote_plus
+# )
+#print(parameterString)
+
+
+percentEncodedURL = percentEncode(baseURL)
+percentEncodedParameterString = percentEncode(parameterString)
+signatureBaseString = "POST&" + percentEncodedURL + "&" + percentEncodedParameterString
+#print(signatureBaseString)
+signingKey = percentEncode(consumer_key_secret) + '&' + percentEncode(access_token_secret)
+#print(signingKey)
+
+def get_signture(data, secret_key):
+    signature = hmac.new(secret_key.encode(), data.encode(), hashlib.sha1).digest()
+    signature = base64.b64encode(signature).decode('utf-8')
+    print(signature)
+    return signature
+
+signature = get_signture(signatureBaseString, signingKey)
+
+oauth_signature_KEY = percentEncode("oauth_signature")
+oauth_signature_VALUE = percentEncode(signature)
+
+def keyValHeader(key,val):
+    return key + "=" + '"' + val + '"'
+
+DST = "OAuth "
+DST += keyValHeader(oauth_consumer_key_KEY, oauth_consumer_key_VALUE) +  ","
+DST += keyValHeader(oauth_token_KEY, oauth_token_VALUE) + ","
+DST += keyValHeader(oauth_signature_method_KEY, oauth_signature_method_VALUE) + ","
+DST += keyValHeader(oauth_timestamp_KEY, oauth_timestamp_VALUE) + ","
+DST += keyValHeader(oauth_nonce_KEY, oauth_nonce_VALUE) + ","
+DST += keyValHeader(oauth_version_KEY, oauth_version_VALUE) + ","
+DST += keyValHeader(oauth_signature_KEY, oauth_signature_VALUE)
+
+url = "https://api.twitter.com/2/tweets"
+
+consumer_key = getKey('api_key')
+access_token = getKey('access_token')
 headers = {
-  'Authorization': f'OAuth oauth_consumer_key={consumer_key},oauth_token={access_token},oauth_signature_method="HMAC-SHA1",oauth_timestamp="1665092513",oauth_nonce="J8MbG3bAETl",oauth_version="1.0",oauth_signature="%2FsHPxD6fG69c1ITrIrRAfjkgnK0%3D"',
+  'Authorization': DST,
   'Content-Type': 'application/json',
   'Cookie': 'guest_id=v1%3A164748160617485457; guest_id_ads=v1%3A164748160617485457; guest_id_marketing=v1%3A164748160617485457; personalization_id="v1_PFHqkQhmwp141oUu4hPm9w=="'
 }
 
-def sendTweet(tweetText, mediaID):
+def sendTweet(tweetText):
     payload = json.dumps({
-        "text": tweetText, 
-        "media": {
-            "media_ids": [mediaID]
-        }
+        "text": tweetText
     })
 
-    requests.request("POST", url, headers=headers, data=payload)
+    print(requests.request("POST", url, headers=headers, data=payload).json())
 
-nonce = secrets.token_urlsafe()
-nonce = ''.join(filter(str.isalnum, nonce))
-timestamp = int(time.time())
-
-DST = "OAuth "
-oauth_consumer_key = os.environ['api_key']
-payload = {"oauth_consumer_key": oauth_consumer_key, "oauth_token":access_token, "oauth_signature_method":"HMAC-SHA1"}
-#print(urlencode(payload, quote_via=quote_plus))
-
-parameterString = urlencode(
-    {
-        "oauth_consumer_key": oauth_consumer_key,
-        "oauth_nonce": nonce,
-        "oauth_signature_method": 'HMAC-SHA1',
-        "oauth_timestamp": timestamp,
-        "oauth_token": access_token,
-        "oauth_version": "1.0"
-    },
-    quote_via=quote_plus
-)
-
-percentEncodedURL = urllib.parse.quote("https://api.twitter.com/2/tweets", safe="")
-percentEncodedParameterString = urllib.parse.quote(parameterString, safe="")
-signatureBaseString = "POST&" + percentEncodedURL + "&" + percentEncodedParameterString
-signingKey = urllib.parse.quote(consumer_key_secret, safe="") + '&' + urllib.parse.quote(access_token_secret, safe="")
-
-signatureBaseString = b'POST&https%3A%2F%2Fapi.twitter.com%2F1.1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521'
-signingKey = b"kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw&LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE"
-
-def get_signture(data, secret_key):
-    signature = hmac.new(secret_key, data, hashlib.sha1).digest()
-    signature = base64.b64encode(signature).decode('utf-8')
-    return signature
-
-get_signture(signatureBaseString, signingKey)
+sendTweet("test all params")
