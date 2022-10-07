3cb2c45701ed41f8b9d9847dcebd2fa27e6c5a13
joesatow <98419512+joesatow@users.noreply.github.com>
2022-10-07 15:37:17 -0400

    added comments

--- helper_funcs/twitterAuth.py
+++ helper_funcs/twitterAuth.py
@@ -6,18 +6,27 @@ import hashlib
 import base64
 from helper_funcs.API_keys import getKey
 
+# Get API keys
 consumer_key = getKey('consumer_key')
 consumer_key_secret = getKey('consumer_key_secret')
 access_token = getKey('access_token')
 access_token_secret = getKey('access_token_secret')
+
+# Base URL
 baseURL = "https://api.twitter.com/2/tweets"
+
+# OAuth nonce
 nonce = secrets.token_urlsafe()
 nonce = ''.join(filter(str.isalnum, nonce))
+
+# Oauth timestamp
 timestamp = str(int(time.time()))
 
+# Function to percent encode any passed in string
 def percentEncode(str):
     return urllib.parse.quote(str, safe="")
 
+# Percent encode every key/value pair
 oauth_consumer_key_KEY = percentEncode("oauth_consumer_key")
 oauth_consumer_key_VALUE = percentEncode(consumer_key)
 
@@ -42,6 +51,7 @@ include_entities_VALUE = percentEncode("true")
 status_KEY = percentEncode("status")
 status_VALUE = percentEncode("Hello Ladies + Gentlemen, a signed OAuth request!")
 
+# Build parameter string
 parameterString = oauth_consumer_key_KEY + '=' + oauth_consumer_key_VALUE + '&'
 parameterString += oauth_nonce_KEY + '=' + oauth_nonce_VALUE + '&'
 parameterString += oauth_signature_method_KEY + '=' + oauth_signature_method_VALUE + '&'
@@ -49,25 +59,32 @@ parameterString += oauth_timestamp_KEY + '=' + oauth_timestamp_VALUE + '&'
 parameterString += oauth_token_KEY + '=' + oauth_token_VALUE + '&'
 parameterString += oauth_version_KEY + '=' + oauth_version_VALUE
 
+# Build signature base string by combining percent encoded base URL and parameter string
 percentEncodedURL = percentEncode(baseURL)
 percentEncodedParameterString = percentEncode(parameterString)
 signatureBaseString = "POST&" + percentEncodedURL + "&" + percentEncodedParameterString
+
+# Build signing key by combining percent encoded consumer secret and token secret
 signingKey = percentEncode(consumer_key_secret) + '&' + percentEncode(access_token_secret)
 
+# Function to hash signature base string and signing key using HMAC-SHA1
 def get_signature(data, secret_key):
     signature = hmac.new(secret_key.encode(), data.encode(), hashlib.sha1).digest()
     signature = base64.b64encode(signature).decode('utf-8')
-    print(signature)
     return signature
 
+# Set OAuth signature
 signature = get_signature(signatureBaseString, signingKey)
 
+# Set final percent encoded key/value pairs needed to build auth header.
 oauth_signature_KEY = percentEncode("oauth_signature")
 oauth_signature_VALUE = percentEncode(signature)
 
+# Helper function for building auth header
 def keyValHeader(key,val):
     return key + "=" + '"' + val + '"'
 
+# Build auth header
 DST = "OAuth "
 DST += keyValHeader(oauth_consumer_key_KEY, oauth_consumer_key_VALUE) +  ","
 DST += keyValHeader(oauth_token_KEY, oauth_token_VALUE) + ","
@@ -77,5 +94,6 @@ DST += keyValHeader(oauth_nonce_KEY, oauth_nonce_VALUE) + ","
 DST += keyValHeader(oauth_version_KEY, oauth_version_VALUE) + ","
 DST += keyValHeader(oauth_signature_KEY, oauth_signature_VALUE)
 
+# Getter method to return auth header
 def getAuthHeader():
     return DST
