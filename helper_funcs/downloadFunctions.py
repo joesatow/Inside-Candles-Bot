276bbdcc6fbf9ba554c7c13b2458283c6c2ad815
joesatow <98419512+joesatow@users.noreply.github.com>
2022-10-21 11:47:31 -0400

    add timeframe to filenames

--- helper_funcs/downloadFunctions.py
+++ helper_funcs/downloadFunctions.py
@@ -9,21 +9,24 @@ from user_agent import generate_user_agent
 sc_cookie = getKey("sc_cookie")
 user_agent = generate_user_agent()
 
-# [0] = Daily, [1] = 4h
-iValues = ['p55738127392', 'p57289512688']
+# [0] = Daily, [1] = 4h, [2] = 1h
+iValues = ['p55738127392', 'p57289512688', 'p23851798625']
 
 def get_chart(symbol, tf):
     if tf == '1d':
         selector = 0
     if tf == '4h':
         selector = 1
+    if tf == '1h':
+        selector = 2
 
     millisecondsEpoch = str(int(time.time()*1000))
 
-    # [0] = Daily, [1] = 4h
+    # [0] = Daily, [1] = 4h, [2] = 1h
     payloadObjects = [
         {"s": symbol, "p": "D", "b": "6", "g": '0', 'i': iValues[selector], 'r': millisecondsEpoch},
-        {"s": symbol, "p:": "195", "b": "6", "g": "0", "i": iValues[selector], 'r': millisecondsEpoch}
+        {"s": symbol, "p:": "195", "b": "6", "g": "0", "i": iValues[selector], 'r': millisecondsEpoch},
+        {"s": symbol, "p:": "60", "b": "6", "g": "0", "i": iValues[selector], 'r': millisecondsEpoch}
     ]
 
     encoded_payload = urlencode(
@@ -32,7 +35,7 @@ def get_chart(symbol, tf):
     
     url = f"https://stockcharts.com/c-sc/sc?{encoded_payload}"
     response = stockCharts_request(url, user_agent)
-    fileName = download_chart_image(response, url)
+    fileName = download_chart_image(response, url, tf)
     return fileName
 
 def stockCharts_request(url: str, user_agent: str) -> Response:
@@ -42,9 +45,9 @@ def stockCharts_request(url: str, user_agent: str) -> Response:
     })
     return response
 
-def download_chart_image(page_content: requests.Response, url,):
+def download_chart_image(page_content: requests.Response, url, tf):
     """ Downloads a .png image of a chart into the "charts" folder. """
-    file_name = f"{url.split('s=')[1].split('&')[0]}_{int(time.time())}.png"
+    file_name = f"{url.split('s=')[1].split('&')[0]}_{int(time.time())}-{tf}.png"
 
     with open(os.path.join("/Library/WebServer/Documents/charts", file_name), "wb") as handle:
         handle.write(page_content.content)
